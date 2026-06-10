fda1a979e86d8f5414eacaae8fd9fe1b6e51cbf3
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-10 12:45:38 +0200

    added combat event class

---
+++ src/main/java/sum3/CombatEvent.java
@@ -0,0 +1,12 @@
+package sum3;
+
+
+// Step 2.2
+
+public class CombatEvent extends TelemetryEvent {
+    // Declare private fields
+
+    // Implement constructor with validation
+
+    // Implement getEventSummary()
+}
