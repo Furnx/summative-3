cc6bcc9bf2df62df3372e9e664f49416bb2c7c0d
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-10 12:49:02 +0200

    added telementry event class - assessment done

---
+++ src/main/java/sum3/TelemetryEvent.java
@@ -0,0 +1,14 @@
+package sum3;
+
+
+// Step 1
+
+public abstract class TelemetryEvent {
+    // Declare private, immutable fields
+
+    // Implement constructor
+
+    // Implement getters
+
+    // Declare abstract getEventSummary() method
+}
