28272137e6df081b28a83bfb47410ea98e9dd38d
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-10 12:47:08 +0200

    added movement event class

---
+++ src/main/java/sum3/MovementEvent.java
@@ -0,0 +1,12 @@
+package sum3;
+
+
+//  Step 2.1
+
+public class MovementEvent extends TelemetryEvent {
+    //  Declare private fields
+
+    // Implement constructor
+
+    //Implement getEventSummary()
+}
